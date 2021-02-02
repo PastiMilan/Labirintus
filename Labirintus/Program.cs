1a462be0d0d1886fb1b6fb1b07956d5bc6c01477
sample <sample@sample.com>
2021-02-02 10:59:28 +0100

    elso

---
+++ Labirintus/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labirintus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class MapHandler
+    {
+        private char[,] map;
+        private String mapname;
+
+        MapHandler(String name)
+        {
+            this.mapname = name;
+        }
+
+        public String getName()
+        {
+            return mapname;
+        }
+
+        public char[,] getMap()
+        {
+            return map;
+        }
+    }
+}
